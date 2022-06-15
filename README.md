88ae74039cfc18d5524d233f7857ca0a4fb11427
Konstantin Wurster <Konstantin.Wurster@zf.com>
2022-06-15 16:31:30 +0200

    working actix + websocket demo

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # anthill
-Exploitlauncher McExploitlauncherface
+
+server is running on `http://localhost:8080`
+
+```bash
+cargo build
+cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-use actix_files::{NamedFile, Files};
-use actix_web::{Responder, HttpServer, App, web};
+use actix_files::{Files, NamedFile};
+use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
 
-
+mod server;
+use actix_web_actors::ws;
+use server::TestWebSocket;
 
 async fn index() -> impl Responder {
-    NamedFile::open_async("./static/index.html").await.ok()
+    NamedFile::open_async("./dist/index.html").await.unwrap()
+}
+
+async fn test_ws(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    ws::start(TestWebSocket::new(), &req, stream)
 }
 
 #[actix_web::main]
@@ -12,7 +18,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .service(web::resource("/").to(index))
-            .service(Files::new("/static", "./static"))
+            .route("/ws", web::get().to(test_ws))
+            .service(Files::new("/", "./dist"))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
