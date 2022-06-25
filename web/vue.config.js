eaa23ee7f309d5558f7b6c43effab40c9a15e34b
spq <spq@aachen.ccc.de>
2022-06-25 16:37:19 +0200

    configure the dev server to forward requests to /api/... to port 8081

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
+  devServer: {
+    proxy: {
+      "^/api/": {
+        target: "http://localhost:8081",
+      },
+    },
+  },
 });
